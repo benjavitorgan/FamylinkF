6183388d76bfd1eecbc0ba39b6dfa1b727b04013
benjavitorgan <vitorganbenjamin@gmail.com>
2023-06-27 15:57:32 -0300

    querys
    Creación de querys para login y registro

--- server.ts
+++ server.ts
@@ -1,10 +1,57 @@
-import { PrismaClient } from '@prisma/client'
-import express from 'express';
+import { getPrismaClient } from '@prisma/client/runtime';
+//import express from 'express';
 
-const prisma = new PrismaClient()
-const app = express();
-const port = 3000;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { old_person } = prisma;
+//const app = express();
+//const port = 3000;
 
+//Funciones con querys
+
+async function CreateUser() {
+    try {
+        const newUser = await old_person.create({
+            data: {
+             dni: "",
+             name: "",
+             lastname: "",
+
+            },
+        });
+        console.log("Usuario creado:", newUser);
+    } catch (error) {
+        console.error ("Error al crear usuario:", error);
+    }
+    finally {await prisma.$disconnect();}
+}
+
+let tdni = 46756340
+
+async function CheckDni () {
+    try {
+        const user = await prisma.old_person.findUnique({
+            where: {
+                dni: tdni,
+            },
+            select: {
+                dni: true,
+                password: true,
+            },
+        });
+
+        if (user) {
+            console.log("Usuario encontrado, bienvenido:", user.dni, user.password);
+        } else {
+            console.log("Usuario no encontrado");
+        } 
+    } catch (error) {
+        console.error ("Error al encontrar usuario:", error);
+    } finally {await prisma.$disconnect();}
+}
+
+// Get y posts
+/*
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -33,3 +80,5 @@ app.post('/api/endpoint', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+*/
